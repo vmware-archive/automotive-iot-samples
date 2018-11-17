ca55bccf1232e578aedd66cf84b34a41770eedbf
Malini Bhandaru <mbhandaru@vmware.com>
2018-11-16 20:22:34 -0800

    update all

--- collect_data.py
+++ collect_data.py
@@ -1,65 +1,37 @@
-import obd
-# import auto_domain_knowledge
-import serial
-import sys
-import time
+
+from auto_domain import Automobile, Edge
 from config import config
+import time
 
-
-SPEED_CMD = obd.commands.SPEED
-RPM_CMD = obd.commands.RPM
-FUEL_LEVEL_CMD = obd.commands.FUEL_LEVEL
-RELATIVE_ACCEL_POS_CMD = obd.commands.RELATIVE_ACCEL_POS
-ABSOLUTE_LOAD_CMD = obd.commands.ABSOLUTE_LOAD
-ENGINE_LOAD_CMD = obd.commands.ENGINE_LOAD
-RELATIVE_THROTTLE_POS_CMD = obd.commands.RELATIVE_THROTTLE_POS
-THROTTLE_POS_B_CMD = obd.commands.THROTTLE_POS_B
-THROTTLE_POS_C_CMD = obd.commands.THROTTLE_POS_C
-
-cmd_list = [
-        SPEED_CMD,
-        RPM_CMD,
-        FUEL_LEVEL_CMD,
-        #RELATIVE_ACCEL_POS_CMD,
-        ABSOLUTE_LOAD_CMD,
-        ENGINE_LOAD_CMD,
-        RELATIVE_THROTTLE_POS_CMD,
-        THROTTLE_POS_B_CMD,
-        #THROTTLE_POS_C_CMD
-        ]
-
-connection = obd.OBD()
-
-max =180000 # alternately forever -- till car stops!
-i = 0
+DEBUG = config["COMMON"]["DEBUG"]
 
 # TODO -- if file already exists, rename the existing file and start a new one
 # TODO -- some error handling would be nice!
 
-data_file = open(DEVICE_DATA_FILE, "w")
-# write the column headers
-headers = "SPEED, RPM, FUEL_LEVEL, ABSOLUTE_LOAD, ENGINE_LOAD, RELATIVE_THROTTLE_POS, THROTTLE_POS_B"
-print(headers)
-data_file.write(headers)
-auto_domain_knowledge.OBD_parser(headers)
+data_filename = config["DEVICE"]["DEVICE_DATA_FILE"]
+# assuming a data push model 
+sampling_frequency = float(config["DEVICE"]["SAMPLING_FREQUENCY"])
+sensors = config["DEVICE"]["SENSORS"]
+print("Collecting and analyzing " + sensors +  " data.")
+print("Sampling at the rate of every " + sampling_frequency +  "  second(s).")
+if DEBUG:
+    print("Echoing all data to: " + data_filename)
 
+
+data_file = open(data_filename, "w")
+# write the column headers
+data_file.write(sensors)
+
+device = Automobile(sensors)
+edge = Edge(sensors)
+i = 0
+max =180000 # alternately forever -- till car stops!
 while (i < max):
     i = i + 1
-    time.sleep(SAMPLING_FREQUENCY)
-    # going through list of commands
-    line_text = str(time.time()) + ", " #new row with time
-    for cmd in cmd_list:
-        # appending results to row
-        try:
-            # TODO add GPS co-ordinates too .. else we shall need to track a separate file and
-            # match up data based on time stamp
-            line_text += str(connection.query(cmd).value.magnitude) + ", "
-        except:
-            line_text +=  "Error, "
-            print("Unexpected error:", sys.exc_info()[0])
+    time.sleep(sampling_frequency)
+    line_text = device.read_sensors()
     print(line_text)
     data_file.write(line_text)
-    auto_domain_knowledge.stream_data(line_text)
-
+    edge.process_stream_data(line_text)
 data_file.close()    
 
--- replay_data.py
+++ replay_data.py
@@ -3,21 +3,29 @@
 # replay the data saved in a file and process as if in streaming mode!
 
 from pathlib import Path
-car_data = Path(DEVICE_DATA_FILE)
-# TODO -- handle exceptions/error conditions
-if car_data.is_file():
-    data_file = open(config.CAR_DATA_FILE, "r")
-    # first line contains table headers, indicates contents
-    table_headers = data_file.readline()
-    # global common variable
-    CAR_DATA_PARSER = OBD_parser(table_headers)
-    if DEBUG: 
-        print(table_headers)
+from config import config
+# plugin pattern for generic functions
+from auto_domain import Edge
 
-    for line in data_file:
-        data_point = data_file.readline()
-        if DEBUG:
-            print(data_point)
-        stream_data(obd_parser.process_data(line))
+
+DEBUG = config["COMMON"]["DEBUG"]
+data_filename = config["DEVICE"]["DEVICE_DATA_FILE"]
+
+# TODO -- handle exceptions/error conditions
+if Path(data_filename).is_file():
+    data_file = open(data_filename, "r", encoding='utf-8-sig')
+    # first line contains table headers, indicates contents
+    sensors_str = data_file.readline()
+    if DEBUG:
+        print("Sensor_str = " + sensors_str)
+    if (sensors_str != "") :
+        edge = Edge(sensors_str)
+        for line in data_file:
+            data_point = data_file.readline()
+            if DEBUG:
+                print(data_point)
+            edge.process_stream_data(line)
+    else:
+        print("Data recording in incorrect format!") 
 else:
     print("No car data to process! Bye!")
