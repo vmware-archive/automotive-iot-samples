32daf70000b9e3ea68656ac8666a798708a22819
Malini Bhandaru <mbhandaru@vmware.com>
2019-01-25 01:26:42 +1300

    Create test_event_to_smart_city.sh
    Simple curl command to submit a smart city event to the smart city cloud endpoint. It should be visible in the dashboard/UI.

---
+++ cloud_API_endpoint/test/test_event_to_smart_city.sh
@@ -0,0 +1,1 @@
+curl -d '{"event_type": "SPEEDING", "event_timestamp": 1541181513, "gps_coord": "37.7992520359445,-122.41955459117891"}' -H "Content-Type: application/json" -X POST http://localhost:2002/add_event
